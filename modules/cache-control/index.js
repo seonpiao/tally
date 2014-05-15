b011ca9ffdad43b85446e6404d77d8c57db67ec2
seon <seon@pulaa.com>
2014-05-15 23:27:56 +0800

    update

--- modules/cache-control/index.js
+++ modules/cache-control/index.js
@@ -1,16 +1,22 @@
+var logger = require('log4js').getLogger('module.cache-control');
+
 module.exports = {
   init: function(app) {
     app.use(function * (next) {
+      var log = this.path + ' is a ';
       //没有扩展名，认为是动态请求，设置为不缓存
       if (this.path.search(/\.[^\.]+$|\?|#/) === -1) {
+        log += 'dynamic resource.';
         this.set('cache-control', 'no-cache');
         this.set('max-age', '0');
+      } else {
+        log += 'static resource.';
       }
+      logger.debug(log);
+      yield next;
     });
   },
   unload: function(app) {
-    this.routes.forEach(function(route) {
-      app.unroute(route);
-    });
+
   }
 }
