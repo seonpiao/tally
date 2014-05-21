3310860a9467a2aa66e8c20bfafc8a34c56128af
seon <seon@pulaa.com>
2014-05-21 22:36:59 +0800

    update

--- dist/template/modules/state_bar/index.js
+++ dist/template/modules/state_bar/index.js
@@ -5,6 +5,6 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 ;var locals_for_with = (locals || {});(function (keyword) {
-buf.push("<div data-module=\"state_bar\" class=\"state_bar\"><span class=\"type\">支出</span><span class=\"keyword\">" + (jade.escape(null == (jade_interp = (typeof keyword !== 'undefined' ? keyword.keyword :'')) ? "" : jade_interp)) + "</span><span class=\"cost\">0</span></div>");}("keyword" in locals_for_with?locals_for_with.keyword:typeof keyword!=="undefined"?keyword:undefined));;return buf.join("");
+buf.push("<div data-module=\"state_bar\" class=\"state_bar\"><span class=\"keyword\">" + (jade.escape(null == (jade_interp = (typeof keyword !== 'undefined' ? keyword.keyword :'')) ? "" : jade_interp)) + "</span><span class=\"type\">支出</span><div class=\"cost\">0</div></div>");}("keyword" in locals_for_with?locals_for_with.keyword:typeof keyword!=="undefined"?keyword:undefined));;return buf.join("");
 };
 })();
