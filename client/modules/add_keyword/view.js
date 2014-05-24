5c46874ebdf4f9dc96c95252083691c7c843401e
seon <seon@pulaa.com>
2014-05-24 09:48:57 +0800

    update

--- client/modules/add_keyword/view.js
+++ client/modules/add_keyword/view.js
@@ -2,13 +2,80 @@ define('popupoverlay-src', 'js/libs/jquery.popupoverlay.js');
 define(["js/views/base", './interfaces/keywordList', 'popupoverlay-src'], function(Base, KeywordList) {
   var View = Base.extend({
     moduleName: "add_keyword",
+    events: {
+      'touchend .btn': '_create',
+      'change #category': '_categoryChange'
+    },
     init: function() {
       new KeywordList({
         module: this
       });
+      this.$keyword = this.$('#keyword');
+      this.$category = this.$('#category');
+      this.$keyword_tip = this.$keyword.next();
+      this.$category_tip = this.$category.next();
+      this._showTip();
     },
     toggle: function() {
       this.$el.toggle();
+    },
+    _showTip: function() {
+      var $tips = this.$('[data-tip]');
+      $tips.each(function(index, tip) {
+        var $tip = $(tip);
+        $tip.html($tip.attr('data-tip'));
+      })
+    },
+    _create: function(e) {
+      e.preventDefault();
+      var keyword = this.$keyword.val().trim();
+      var category = this.$category.val();
+      if (keyword === '') {
+        this._keywordError('必须输入消费项目');
+        return;
+      }
+      if (this.$category.prop('selectedIndex') === 0) {
+        this._categoryError('必须选择一个分类');
+        return;
+      }
+      location.href = '/cost/' + keyword + '/' + category;
+    },
+    _categoryChange: function() {
+      var category = this.$category.val();
+      var self = this;
+      if (category === '创建新分类') {
+        category = prompt('输入分类名').trim();
+        if (category) {
+          var $option = $('<option>' + category + '</option>');
+          $option.innerText = category;
+          setTimeout(function() {
+            $option.insertBefore(self.$category.find('option').last());
+            self.$category[0].selectedIndex = self.$category.find('option').length - 2;
+          }, 0);
+        }
+      }
+    },
+    _keywordError: function(msg) {
+      this.$keyword.addClass('error');
+      this.$keyword_tip.addClass('error');
+      this.$keyword_tip.html(msg);
+      var self = this;
+      setTimeout(function() {
+        self.$keyword.removeClass('error');
+        self.$keyword_tip.removeClass('error');
+        self._showTip();
+      }, 3000);
+    },
+    _categoryError: function(msg) {
+      this.$category.addClass('error');
+      this.$category_tip.addClass('error');
+      this.$category_tip.html(msg);
+      var self = this;
+      setTimeout(function() {
+        self.$category.removeClass('error');
+        self.$category_tip.removeClass('error');
+        self._showTip();
+      }, 3000);
     }
   });
   return View;
--- dist/template/modules/add_keyword/index.js
+++ dist/template/modules/add_keyword/index.js
@@ -5,7 +5,7 @@ var buf = [];
 var jade_mixins = {};
 var jade_interp;
 ;var locals_for_with = (locals || {});(function (categories, decodeURIComponent) {
-buf.push("<div data-module=\"add_keyword\" class=\"add_keyword\"><input id=\"keyword\" type=\"text\" placeholder=\"关键字\"/><select id=\"category\"><option>选择分类</option>");
+buf.push("<div data-module=\"add_keyword\" class=\"add_keyword\"><div class=\"row\"><input id=\"keyword\" type=\"text\" placeholder=\"消费项目\"/><span data-tip=\"例如：午餐、加油\" class=\"tip\"></span></div><div class=\"row\"><select id=\"category\"><option>选择分类</option>");
 // iterate categories
 ;(function(){
   var $$obj = categories;
@@ -28,6 +28,6 @@ buf.push("<option>" + (jade.escape(null == (jade_interp = decodeURIComponent(ite
   }
 }).call(this);
 
-buf.push("<option>新分类</option></select><a onclick=\"javascript:if(document.getElementById('category').selectedIndex === 0){alert('必须选择一个分类');return;}if(document.getElementById('keyword').value === ''){alert('必须输入关键词');return;}location.href='/cost/' + document.getElementById('keyword').value + '/' + document.getElementById('category').value\" class=\"btn small-btn\">确定</a></div>");}("categories" in locals_for_with?locals_for_with.categories:typeof categories!=="undefined"?categories:undefined,"decodeURIComponent" in locals_for_with?locals_for_with.decodeURIComponent:typeof decodeURIComponent!=="undefined"?decodeURIComponent:undefined));;return buf.join("");
+buf.push("<option class=\"create\">创建新分类</option></select><span data-tip=\"例如：餐饮、交通\" class=\"tip\"></span></div><div class=\"row\"><a class=\"btn small-btn\">确定</a></div></div>");}("categories" in locals_for_with?locals_for_with.categories:typeof categories!=="undefined"?categories:undefined,"decodeURIComponent" in locals_for_with?locals_for_with.decodeURIComponent:typeof decodeURIComponent!=="undefined"?decodeURIComponent:undefined));;return buf.join("");
 };
 })();
